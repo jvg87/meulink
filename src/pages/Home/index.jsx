c153b69468efa698a0525a39b04e2f1512d130a6
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-04-10 13:51:43 -0300

    finalizando estilos da home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,25 @@
+import {FiLink} from 'react-icons/fi'
+import './home.css'
 
 export default function Home(){
     return (
-    <div>
-        <h1>Pagina Home!</h1>
+    <div className="container-home">
+        <div className="logo">
+            <img src="/slave.png" alt="link logo" />
+            <h1>Meu Link</h1>
+            <span>Cole seu link para encurtar</span>
+        </div>
+
+        <div className="area-input">
+            <div>
+                <FiLink size={24} color='#fff'/>
+                <input 
+                    type="text"
+                    placeholder='Cole seu link aqui...'    
+                />
+            </div>
+            <button>Encurtar Link</button>
+        </div>
     </div>
     )
 }
